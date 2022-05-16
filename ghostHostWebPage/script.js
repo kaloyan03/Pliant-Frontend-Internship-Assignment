cc12b8ca858083658ba5c967516a911f8f4c13be
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-05-16 23:30:03 +0300

    almost ready with the project, mobile nav on click implementated, desktop version almost ready too

---
+++ ghostHostWebPage/script.js
@@ -0,0 +1,14 @@
+function showMobileNav() {
+    let mobileNavBtn = document.querySelector('.mobile-menu-btn');
+
+    mobileNavBtn.addEventListener('click', (e) => {
+        let mobileMenu = document.querySelector('.header-nav');
+        if (mobileMenu.classList.contains('open')) {
+            mobileMenu.classList.remove('open');
+        } else {
+            mobileMenu.classList.add('open');
+        }
+    })
+}
+
+showMobileNav();
